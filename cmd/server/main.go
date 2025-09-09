64addb079f8c39ce5924ce665b203fbd5ad891be
ivanruzin <ruzinivan1@gmail.com>
2025-09-09 19:07:48 +0300

    PZ1: minimal HTTP server + uuid

---
+++ cmd/server/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type user struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+func main() {
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("Hello, world!"))
+		if err != nil {
+			log.Panic(err.Error())
+		}
+	})
+
+	mux.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		_ = json.NewEncoder(w).Encode(user{
+			ID:   uuid.NewString(),
+			Name: "Gopher",
+		})
+	})
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		_ = json.NewEncoder(w).Encode(map[string]string{
+			"status": "ok",
+			"time":   time.Now().Format(time.RFC3339),
+		})
+	})
+
+	addr := ":" + port
+	log.Printf("Starting on %s ...", addr)
+	log.Fatal(http.ListenAndServe(addr, mux))
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module tip_pr1
+
+go 1.25.1
+
+require github.com/google/uuid v1.6.0
